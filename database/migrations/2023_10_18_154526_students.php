26cda63e2c368ee39c62ab0fd1e445b2d6eebfdc
Samuel Rodriguez <samuel_rodriguez_p@hotmail.com>
2023-10-19 11:15:20 -0600

    migration Students&Persons

--- database/migrations/2023_10_18_154526_students.php
+++ database/migrations/2023_10_18_154526_students.php
@@ -11,8 +11,28 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
-    }
+        Schema::create('students', function(Blueprint $table){
+
+            $table->id();
+            $table->string('Name');
+            $table->string('Last');
+            $table->string('Las2');
+            $table->char('Gender');
+            $table->string('CURP');
+            $table->string('MaritalStatusID');
+            $table->date('BirthDate');
+            $table->string('BirthCity');
+            $table->id('BirthPlaceID');
+            $table->id('ReligionID');
+            $table->string('PrimaryEMail');
+            $table->integer('CellPhone');
+            $table->id('NationalityID');
+            $table->id('ProgramID');
+            $table->id('TermID');
+            $table->id('ClassLevelID');
+            $table->id('GroupID');
+            $table->timestamps();
+        });}
 
     /**
      * Reverse the migrations.
--- database/migrations/2023_10_18_154541_persons.php
+++ database/migrations/2023_10_18_154541_persons.php
@@ -11,7 +11,24 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('persons', function(Blueprint $table){
+
+            $table->id();
+            $table->string('Name');
+            $table->string('Last');
+            $table->string('Last2');
+            $table->char('Gender');
+            $table->date('BirthDate');
+            $table->id('BirthPlaceID');
+            $table->string('BirthCity');
+            $table->string('CURP');
+            $table->integer('CellPhone');
+            $table->id('PrimaryEmail');
+            $table->id('MaritalStatusID');
+            $table->string('WorkPlace');
+            $table->string('WorkJob');
+            $table->timestamps();
+        });
     }
 
     /**
